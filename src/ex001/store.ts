865589c9a51b49e8a0070d27596330a7c52c5c87
Christopher L Crum <flycrum@Christophers-MacBook-Pro.local>
2020-06-21 00:25:02 -0500

    refactor: simplify example 001 boilerplate

--- src/ex001/store.ts
+++ src/ex001/store.ts
@@ -1,19 +1,17 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const slice = createSlice({
-    name: `Example001`,
-    initialState: {
-        count: 1,
-    },
-    reducers: {},
-});
-
 /**
  * Foregoing rootReducer and instead passing the slice reducers directly to configureStore().
  */
 export const store = configureStore({
     reducer: {
-        example: slice.reducer,
+        example: createSlice({
+			name: `Example001`,
+			initialState: {
+				count: 1,
+			},
+			reducers: {},
+		}).reducer,
     },
 });
 
