b8d43042aafd08f1df0091557ecbdece5f225b9e
Christopher L Crum <flycrum@Christophers-MacBook-Pro.local>
2020-06-22 10:58:03 -0500

    feat: improve ex500

--- src/ex500/Ex500.tsx
+++ src/ex500/Ex500.tsx
@@ -1,30 +1,39 @@
 import React from 'react';
-import { store, useExampleSelector, ExampleStateType, incrementAfterAsyncEffectThunk } from './store';
+import { store, useExampleSelector, storeExample500 } from './store';
 import Spinner from '../shared/components/Spinner';
 
-export default function Ex005() {
-	const { count, isLoading } = useExampleSelector((state: ExampleStateType) => state.example);
+export default function Ex500() {
+	const { count, isLoading } = useExampleSelector((state) => state.example);
 
 	return (
 		<div>
-			<button onClick={() => store.dispatch(incrementAfterAsyncEffectThunk(1))}>
-				Increment:
-				{ ' ' }
-				{ count }
-				{ isLoading && (
+			{/* use convenience 'request' list of thunks automatically wrapped in the store dispatch */}
+			<button onClick={() => storeExample500.slice.request.decrementAfterAsyncEffectThunk(500)}>
+				- Decrement
+			</button>
+			{ ' ' }
+			{ count }
+			{ isLoading && (
+				<span style={{ display: 'inline-block', width: '40px', height: '10px'}}>
 					<Spinner />
-				)}
+				</span>
+			)}
+			{ ' ' }
+			{/*<button onClick={() => yoyo.slice.dispatch.increment(102)}>*/}
+			<button onClick={() => storeExample500.slice.request.incrementAfterAsyncEffectThunk(500)}>
+				+ Increment
 			</button>
 		</div>
 	);
 }
 
-Ex005.store = store;
-Ex005.description = `
-A magical, yet unproven, way to bundle more of a slice into a single definition
-to allow for built-in helpers, typings, and even inline thunks.
+Ex500.store = store;
+Ex500.description = `
+A magical, yet unproven and highly experimental, way to bundle more of a slice into a single definition
+to allow for built-in helpers, typings, and even inline thunk calls.
 
 Added properties and methods:
   - dispatch: wraps actions in store.dispatch
-  -
+  - thunks: list of thunk definitions that automatically wrap functions in auto-named 'createAsyncThunk'
+  - think: wraps thunks in store.dispatch (todo: not yet typed)
 `;
--- src/ex500/store.ts
+++ src/ex500/store.ts
@@ -14,19 +14,41 @@ import sleep from '../shared/helpers/sleep';
 
 const createSlicePlus = <State, CaseReducers extends SliceCaseReducers<State>, Name extends string = string>(
 	state: {
-		name: Name
+		extraReducers: any
 		initialState: State
+		name: Name
 		reducers: ValidateSliceCaseReducers<State, CaseReducers>
+		thunks: any
 	}
 ) => {
 	const slice = createSlice(state);
 	const dispatchActions: {[key: string]: Function} = {};
+	const dispatchThunks: {[key: string]: Function} = {};
 
 	for (const key in slice.actions) {
 		if (slice.actions.hasOwnProperty(key)) {
 			// wrap action within store dispatch
 			dispatchActions[key] = (...args: any[]) => {
-				store.dispatch((slice.actions[key] as Function)(...args));
+				return store.dispatch((slice.actions[key] as Function)(...args));
+			}
+		}
+	}
+
+	// for (const key in state.thunks) {
+	// 	if (state.thunks.hasOwnProperty(key)) {
+	// 		// wrap action within store dispatch
+	// 		state.thunks[key] = createAsyncThunk(
+	// 			`${state.name}\\${key}`,
+	// 			state.thunks[key],
+	// 		)
+	// 	}
+	// }
+
+	for (const key in state.thunks) {
+		if (state.thunks.hasOwnProperty(key)) {
+			// wrap action within store dispatch
+			dispatchThunks[key] = async (...args: any[]) => {
+				return await store.dispatch((state.thunks[key] as Function)(...args));
 			}
 		}
 	}
@@ -38,43 +60,98 @@ const createSlicePlus = <State, CaseReducers extends SliceCaseReducers<State>, N
 		dispatch: dispatchActions as typeof slice.actions,
 		name: slice.name,
 		reducer: slice.reducer,
+		// thunks: state.thunks, // todo - work on typing
+		// add request prop that pre-wraps thunks within store dispatch
+		request: dispatchThunks as typeof state.thunks, // todo - work on typing
 	}
 };
 
-const slice = createSlicePlus({
-	name: 'Example500',
-	initialState: {
-		count: 0,
-		isLoading: false,
-	},
-	reducers: {
-		increment(state, action: PayloadAction<number>) {
-			state.count += action.payload;
+function StoreExample500() {
+	const name = StoreExample500.name;
+
+	/**
+	 * A thunk that directly and internally dispatches actions.
+	 */
+	const incrementAfterAsyncEffectThunk = createAsyncThunk(
+		`${name}/incrementAfterAsyncEffectThunk`,
+		// Declare the type your function argument here:
+		async (num: number) => {
+			slice.dispatch.setLoading(true);
+			await sleep(1000);
+			slice.dispatch.increment(num);
+			slice.dispatch.setLoading(false);
+		}
+	)
+
+	/**
+	 * Another thunk that simply returns data and offloads handling of lifecycle to extraReducers and case builder.
+	 */
+	const decrementAfterAsyncEffectThunk = createAsyncThunk(
+		`${name}/decrementAfterAsyncEffectThunk`,
+		// Declare the type your function argument here:
+		async (num: number) => {
+			await sleep(1000);
+			return num;
+		}
+	)
+
+	const slice = createSlicePlus({
+		name,
+		initialState: {
+			count: 0,
+			isLoading: false,
+			requestIdLast: undefined,
 		},
-		setLoading(state, { payload: isLoading }: PayloadAction<boolean>) {
-			state.isLoading = isLoading;
+		reducers: {
+			increment(state, action: PayloadAction<number>) {
+				state.count += action.payload;
+			},
+			setLoading(state, { payload: isLoading }: PayloadAction<boolean>) {
+				state.isLoading = isLoading;
+				// this does not handle queued requests and the loader will get cancelled out with back-to-back calls
+			},
 		},
-	},
-});
+		extraReducers(builder: any) { // todo - type
+			builder.addCase(decrementAfterAsyncEffectThunk.pending, (state: any, {meta}: any) => {
+				state.isLoading = true;
+				state.requestIdLast = meta.requestId;
+			})
+			builder.addCase(decrementAfterAsyncEffectThunk.fulfilled, (state: any, {payload, meta}: any) => {
+				state.count -= payload;
+
+				// only do if latest request as this will prevent back-to-back calls colliding and stopping the loader prematurely
+				if (meta.requestId === state.requestIdLast) {
+					// if trying to call another action, it must be delayed (is there a better way of doing this?)
+					setTimeout(() => slice.dispatch.setLoading(false));
+				}
+			})
+			builder.addCase(decrementAfterAsyncEffectThunk.rejected, (state: any, {meta}: any) => {
+				if (meta.requestId === state.requestIdLast) {
+					state.isLoading = false;
+				}
+			})
+		},
+		/**
+		 * Include thunks here to generate wrapped dispatches.
+		 */
+		thunks: {
+			decrementAfterAsyncEffectThunk,
+			incrementAfterAsyncEffectThunk,
+		}
+	});
+
+	return {
+		slice,
+	}
+}
+
+export const storeExample500 = StoreExample500();
 
 export const store = configureStore({
 	reducer: {
-		example: slice.reducer,
+		example: storeExample500.slice.reducer,
 	},
 });
-
-// Define a thunk that dispatches those action creators
-export const incrementAfterAsyncEffectThunk = createAsyncThunk(
-	`${slice.name}/incrementAfterAsyncEffectThunk`,
-	// Declare the type your function argument here:
-	async (num: number) => {
-		console.log('lalal');
-		slice.dispatch.setLoading(true);
-		await sleep(1000);
-		slice.dispatch.increment(num);
-		slice.dispatch.setLoading(false);
-	}
-)
 
 export type ExampleStateType = ReturnType<typeof store.getState>;
 export const useExampleSelector = useSelector as TypedUseSelectorHook<ExampleStateType>;
